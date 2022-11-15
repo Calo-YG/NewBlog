c62d9e0e61bbdb0f0185796644fcb2bd985b0e0c
YG-Calo <3164522206@qq.com>
2022-11-15 22:26:16 +0800

    DbExtensions更改

--- Calo.Blog.EntityCore/DataBase/Extensions/DBExtentsions.cs
+++ Calo.Blog.EntityCore/DataBase/Extensions/DBExtentsions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using SqlSugar;
 using System;
 using System.Collections.Generic;
@@ -14,10 +15,30 @@ namespace Calo.Blog.EntityCore.DataBase.Extensions
         public static IServiceCollection AddSqlSugarDbContext<TDbContext>(this IServiceCollection services,Action<ConnectionConfig> action,ServiceLifetime lifetime=ServiceLifetime.Transient) where TDbContext:IDbContext
         {
             ConnectionConfig config=new ConnectionConfig();
+            SqlSugarScope scope;
             action.Invoke(config);
             if (lifetime == ServiceLifetime.Scoped)
             {
-                //services.AddScoped<ISqlSugarClient,SqlSugarScope>(p)；
+                services.AddScoped<ISqlSugarClient, SqlSugarScope>(p =>
+                {
+                    scope= new SqlSugarScope(config);
+                    return scope;
+                });
+            }else if(lifetime == ServiceLifetime.Singleton) 
+            {
+                services.AddSingleton<ISqlSugarClient, SqlSugarScope>(p =>
+                {
+                    scope = new SqlSugarScope(config);
+                    return scope;
+                });
+            }
+            else
+            {
+                services.AddTransient<ISqlSugarClient, SqlSugarScope>(p =>
+                {
+                    scope = new SqlSugarScope(config);
+                    return scope;
+                });
             }
             return services;
         }

448127c238ea4fcca636bc13798df9366f8c2cfb
wyg <3164522206@qq.com>
2022-12-27 15:51:22 +0800

    泛型仓储注入

--- Calo.Blog.EntityCore/DataBase/Repository/RepositoryDependencyInjection.cs
+++ Calo.Blog.EntityCore/DataBase/Repository/RepositoryDependencyInjection.cs
@@ -12,19 +12,19 @@ namespace Calo.Blog.EntityCore.DataBase.Repository
 
         public Type RepositoryInterfaceWithPrimaryKey { get; }
 
-        //public Type RepositoryImplementation { get; }
+        public Type RepositoryImplementation { get; }
 
-        //public Type RepositoryImplementationWithPrimaryKey { get; }
+        public Type RepositoryImplementationWithPrimaryKey { get; }
 
         public RepositoryDependencyInjection(Type repositoryInterface,
-            Type repositoryInterfaceWithPrimaryKey)
-            //Type repositoryImplementation,
-            //Type repositoryImplementationWithPrimaryKey)
+            Type repositoryInterfaceWithPrimaryKey,
+            Type repositoryImplementation,
+            Type repositoryImplementationWithPrimaryKey)
         {
             RepositoryInterface = repositoryInterface;
-           RepositoryInterfaceWithPrimaryKey = repositoryInterfaceWithPrimaryKey;
-        //    RepositoryImplementation = repositoryImplementation;
-        //    RepositoryImplementationWithPrimaryKey = repositoryImplementationWithPrimaryKey;
+            RepositoryInterfaceWithPrimaryKey = repositoryInterfaceWithPrimaryKey;
+            RepositoryImplementation = repositoryImplementation;
+            RepositoryImplementationWithPrimaryKey = repositoryImplementationWithPrimaryKey;
         }
     }
 }

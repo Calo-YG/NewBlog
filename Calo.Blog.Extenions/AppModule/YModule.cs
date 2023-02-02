1f82553a7f0fff44a57c1b08555c8e556e832561
wyg <3164522206@qq.com>
2023-02-02 17:46:55 +0800

    添加YModule扩展方法

--- Calo.Blog.Extenions/AppModule/YModule.cs
+++ Calo.Blog.Extenions/AppModule/YModule.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ Calo.Blog.Extenions/AppModule/YModuleExtenstion.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calo.Blog.Extenions.AppModule
+{
+    public static class YModuleExtenstion
+    {
+        public static IConfiguration Configuration(IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetRequiredService<IConfiguration>();
+        }
+    }
+}

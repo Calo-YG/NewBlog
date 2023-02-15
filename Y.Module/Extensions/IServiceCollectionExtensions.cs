bbbf6c2ca3529731a905e6b678d64fa3a6040e29
wyg <3164522206@qq.com>
2023-02-15 18:18:48 +0800

    IServiceColletion静态类编写

--- Y.Module/Extensions/IServiceCollectionExtensions.cs
+++ Y.Module/Extensions/IServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,10 @@ namespace Y.Module.Extensions
 {
     public static class IServiceCollectionExtensions
     {
+        public static void TryAddObjectAccessor<T>(this IServiceCollection services, T obj)
+        {
+            if (services.Any(p => p.ServiceType == typeof(T))) return;
+
+        }
     }
 }

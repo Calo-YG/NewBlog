69af781270ee4a54cc9174f1ecb8828acf28a452
YG-Calo <3164522206@qq.com>
2023-02-16 01:05:32 +0800

    StartModule类删除，扩展IServiceCollection方法

--- Y.Module/Extensions/IServiceCollectionExtensions.cs
+++ Y.Module/Extensions/IServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Y.Module.Interfaces;
 
 namespace Y.Module.Extensions
 {
@@ -13,7 +14,33 @@ namespace Y.Module.Extensions
         public static void TryAddObjectAccessor<T>(this IServiceCollection services, T obj)
         {
             if (services.Any(p => p.ServiceType == typeof(T))) return;
+            var objectAccessor = new ObjectAccessor<T>(obj);
+            services.Insert(0, ServiceDescriptor.Singleton<IObjectAccessor<T>>( objectAccessor));
+        }
 
+        public static bool IsExists(this IServiceCollection services,Type type)
+        {
+           return services.Any(p=>p.ServiceType == type);
+        }
+
+        public static bool IsExists<T>(this IServiceCollection services) 
+        {
+          return services.IsExists(typeof(T));
+        }
+
+        public static T? GetSingletonOrNull<T>(this IServiceCollection services)
+        {
+            return (T?)services.FirstOrDefault(p=>p.ServiceType==typeof(T))?.ImplementationInstance;
+        }
+
+        public static T GetSingleton<T>(this IServiceCollection services)
+        {
+            var obj = GetSingletonOrNull<T>(services);
+            if (obj is  null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            return obj;
         }
     }
 }

aeeb7233e275f31ac611454d258c919e0ff8cee1
YG-Calo <3164522206@qq.com>
2023-02-18 10:32:31 +0800

    项目启动文件

--- Calo.Blog.Host/Program.cs
+++ Calo.Blog.Host/Program.cs
@@ -2,14 +2,7 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Calo.Blog.Host;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerUI;
-using System;
-using System.IO;
-using System.Reflection;
 using Y.Module.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);

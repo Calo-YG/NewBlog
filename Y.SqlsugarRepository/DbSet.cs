6c630300fa7223f08a98435edec55adc365a2bb6
wyg <3164522206@qq.com>
2023-03-07 16:13:18 +0800

    修改DbSet

--- Y.SqlsugarRepository/DbSet.cs
+++ Y.SqlsugarRepository/DbSet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,5 @@ namespace Y.SqlsugarRepository
 {
     public class DbSet<T> : IDbSet<T>
     {
-        public T Entity { get; set; }
     }
 }
--- Y.SqlsugarRepository/IDbSet.cs
+++ Y.SqlsugarRepository/IDbSet.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 
 namespace Y.SqlsugarRepository
 {
-    public interface IDbSet<T>:ISugarQueryable<T>
+    public interface IDbSet<T>
     {
-        public T Entity { get; set; }
     }
 }

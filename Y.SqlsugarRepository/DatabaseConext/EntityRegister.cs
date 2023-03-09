7763d34ab3777fc39f9cfc403cc97915c19159cf
wyg <3164522206@qq.com>
2023-03-09 11:06:01 +0800

    添加数据库实体注册程序

---
+++ Y.SqlsugarRepository/DatabaseConext/EntityRegister.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y.SqlsugarRepository.DatabaseConext
+{
+    /// <summary>
+    /// 数据库实体注册程序
+    /// </summary>
+    public class EntityRegister : IEntityRegister
+    {
+    }
+}

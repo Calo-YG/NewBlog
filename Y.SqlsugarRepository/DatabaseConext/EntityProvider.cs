685181d5dcb04b9d0fecfab73881a2e77d3b3d40
wyg <3164522206@qq.com>
2023-03-09 14:16:56 +0800

    修改数据库实体提供程序

--- Y.SqlsugarRepository/DatabaseConext/EntityProvider.cs
+++ Y.SqlsugarRepository/DatabaseConext/EntityProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,27 +10,53 @@ namespace Y.SqlsugarRepository.DatabaseConext
     /// <summary>
     /// 数据库实体类型提供类
     /// </summary>
-    public class EntityProvider
+    public class EntityProvider : IEntityProvider
     {
         public Type[] Entitys { get => _entitys.ToArray(); }
 
+        public IServiceCollection Services { get; set; }
+
         private List<Type> _entitys { get; set; }
 
-        public EntityProvider(List<Type> entitys)
+        public EntityProvider(IServiceCollection services
+            , List<Type> entitys)
         {
             _entitys = entitys;
+            Services = services;
+            _entitys = new List<Type>();
+            services.AddSingleton<IEntityProvider>(this);
         }
 
         static EntityProvider() { }
 
         public void Add(Type type)
         {
+            CheckEntity(type);
             _entitys.Add(type);
         }
 
         public void Add<TEntity>()
         {
+            CheckEntity<TEntity>();
             _entitys.Add(typeof(TEntity));
         }
+
+        public IEntityProvider AddEnity(Type type)
+        {
+            CheckEntity(type);
+            _entitys.Add(type);
+            return this;
+        }
+
+        public IEntityProvider AddEnity<TEntity>()
+        {
+            CheckEntity<TEntity>();
+            _entitys.Add(typeof(TEntity));
+            return this;
+        }
+
+        private void CheckEntity(Type type) { }
+
+        private void CheckEntity<TEntity>() { }
     }
 }
---
+++ Y.SqlsugarRepository/DatabaseConext/IEntityProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y.SqlsugarRepository.DatabaseConext
+{
+    public interface IEntityProvider
+    {
+        /// <summary>
+        /// 数据库实体
+        /// </summary>
+        public Type[] Entitys { get; }
+
+        public IServiceCollection Services { get; set; }
+        /// <summary>
+        /// 添加数据库实体类型
+        /// </summary>
+        /// <param name="type"></param>
+        void Add(Type type);
+        /// <summary>
+        /// 添加数据库实体类型
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        void Add<TEntity>();
+        /// <summary>
+        /// 添加数据库实体类型
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        IEntityProvider AddEnity(Type type);
+        /// <summary>
+        /// 添加数据库实体类型
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        IEntityProvider AddEnity<TEntity>();
+    }
+}

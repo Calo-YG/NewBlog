c9dafafc27dfa5d59ff240ac0bba3712604c41ef
YG-Calo <3164522206@qq.com>
2023-03-13 22:45:59 +0800

    修改SqlSugarEntityCoreModule

--- Calo.Blog.EntityCore/SqlSugarEnityCoreModule.cs
+++ Calo.Blog.EntityCore/SqlSugarEnityCoreModule.cs
@@ -1,4 +1,5 @@
 ﻿using Calo.Blog.EntityCore.DataBase.Entities;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Y.Module;
 using Y.Module.Extensions;
@@ -27,14 +28,14 @@ namespace Calo.Blog.EntityCore
             context.Services.AddScoped<IDbAopProvider, DbAopProvider>();
             context.Services.AddSingleton<IEntityManager,EntityManager>();
             //添加数据库上下文AOP配置
-            //Configure<DbConfigureOptions>(options =>
-            //{
-            //    var config = configuration
-            //    .GetSection("App:DbConfigureOptions")
-            //    .Get<DbConfigureOptions>();
-            //    options.EnableAopLog = config.EnableAopLog;
-            //    options.EnableAopError = config.EnableAopError;
-            //});
+            Configure<DbConfigureOptions>(options =>
+            {
+                var config = configuration
+                .GetSection("app:dbconfigureoptions")
+                .Get<DbConfigureOptions>();
+                options.EnableAopLog = config.EnableAopLog;
+                options.EnableAopError = config.EnableAopError;
+            });
             context.Services.AddRepository(provider =>
             {
                 //添加数据库实体

127620bc3991fa0254bec377c8798123c4a87bd6
wyg <3164522206@qq.com>
2023-03-14 16:50:21 +0800

    添加批量注入

--- Y.Module/DependencyInjection/InjectionFactory.cs
+++ Y.Module/DependencyInjection/InjectionFactory.cs
@@ -1,41 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Y.Module.Extensions;
 
 namespace Y.Module.DependencyInjection
 {
     public class InjectionFactory
     {
-        public void InjectionAssembly(Assembly assembly)
+        public void InjectionAssembly(IServiceCollection service, Assembly assembly)
         {
-            var injectionTypes = new List<Type>()
-            {
-                typeof(ISingletonInjection)
-                ,typeof(IScopedInjection)
-                ,typeof(ITransientInjection)
-            };
             var types = assembly.GetTypes();
             foreach (var type in types)
             {
                 var firstInterface = type.GetInterfaces().FirstOrDefault();
-                if (firstInterface != null && !injectionTypes.Contains(firstInterface))
-                {
+                if (firstInterface is null || service.IsExists(firstInterface)) continue;
 
-                }
+
             }
         }
-        protected virtual Type? GetInterfaceTypeByInjectionAttribute(Type type)
+        protected virtual void InjectionAttribute(IServiceCollection services, Type type)
         {
             var attributes = type.GetCustomAttributes().Cast<InjectionAttribute>();
             var injectionAttribute = attributes.FirstOrDefault();
             if (!attributes.Any() || injectionAttribute is null || injectionAttribute.InterfaceType is null)
             {
-                return null;
+                if (!services.IsExists(type))
+                {
+                    AddToInjection(services, type, injectionAttribute.InjectionEnum);
+                }
             }
-            return injectionAttribute.InterfaceType;
+            if (injectionAttribute != null && !services.IsExists(injectionAttribute.InterfaceType))
+            {
+                AddIoInjectionWithInterface(services, type, injectionAttribute.InterfaceType, injectionAttribute.InjectionEnum);
+            }
+        }
+
+        private void AddToInjection(IServiceCollection services, Type type, InjectionEnum injectionEnum)
+        {
+            if (injectionEnum == InjectionEnum.Singleton)
+            {
+                services.AddSingleton(type);
+            }
+            if (injectionEnum == InjectionEnum.Scoped)
+            {
+                services.AddScoped(type);
+            }
+            if (injectionEnum == InjectionEnum.Transient)
+            {
+                services.AddTransient(type);
+            }
+        }
+
+        private void AddIoInjectionWithInterface(IServiceCollection services, Type type, Type interfaeType, InjectionEnum injectionEnum)
+        {
+            if (injectionEnum == InjectionEnum.Singleton)
+            {
+                services.AddSingleton(interfaeType, type);
+            }
+            if (injectionEnum == InjectionEnum.Scoped)
+            {
+                services.AddScoped(interfaeType, type);
+            }
+            if (injectionEnum == InjectionEnum.Transient)
+            {
+                services.AddTransient(interfaeType, type);
+            }
         }
     }
 }

d3025ae5a58acfdb5b616eec749935b8ba7ab43b
WYG <3164522206@qq.com>
2023-07-21 18:07:36 +0800

    添加minio基础配置

---
+++ Calo.Blog.Common/Minio/MinioConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace Calo.Blog.Common.Minio
+{
+	public class MinioConfig
+	{
+		/// <summary>
+		/// minio服务器地址
+		/// </summary>
+		public string Host { get; set; }
+		/// <summary>
+		/// 用户名
+		/// </summary>
+		public string AccessKey { get; set; }	
+		/// <summary>
+		/// 密码
+		/// </summary>
+		public string SecretKey { get; set; }
+		/// <summary>
+		/// 默认存储桶名称
+		/// </summary>
+		public string DefaultBucket { get; set; }
+		/// <summary>
+		/// 用户名
+		/// </summary>
+		public string UserName { get; set; }
+		/// <summary>
+		/// 密码
+		/// </summary>
+		public string Password { get; set; }	
+		/// <summary>
+		/// 操作面板
+		/// </summary>
+		public string Protal { get; set; }
+	}
+}
---
+++ Calo.Blog.Common/Minio/MinioExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Minio;
+
+namespace Calo.Blog.Common.Minio
+{
+	public static class MinioExtensions
+	{
+		/// <summary>
+		/// ssl 密钥创建minio链接
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="options"></param>
+		public static void AddMinio(IServiceCollection services,Action<MinioConfig> options)
+		{
+			var config = new MinioConfig();
+
+			options.Invoke(config);
+
+		    var client = new MinioClient()
+				.WithEndpoint(config.Host)
+				.WithCredentials(config.AccessKey,config.SecretKey)
+				.WithSSL()
+				.Build();
+
+			services.AddSingleton<MinioClient>(client);
+		}
+	}
+}

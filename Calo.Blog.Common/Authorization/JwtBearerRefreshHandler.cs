a4bc699491dcdf6d93a2bd6668907d418564e271
YG-Calo <3164522206@qq.com>
2023-10-25 18:48:49 +0800

    jwt 滑动刷新方案

--- Calo.Blog.Common/Authorization/JwtBearerRefreshHandler.cs
+++ Calo.Blog.Common/Authorization/JwtBearerRefreshHandler.cs
@@ -2,20 +2,37 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System.Text.Encodings.Web;
 
 namespace Calo.Blog.Common.Authorization
 {
-    public class JwtBearerRefreshHandler : JwtBearerHandler
+    public class JwtBearerRefreshHandler : AuthenticationHandler<JwtBearerRefreshOptions>
     {
+        private OpenIdConnectConfiguration? _configuration;
+
+        protected new JwtBearerRefreshEvents RefreshEvents
+        {
+            get => (JwtBearerRefreshEvents)base.Events!;
+            set => base.Events = value;
+        }
+
+        protected new  readonly JwtBearerEvents Events;
+
         private readonly JwtBearerRefreshOptions _options;
-        public JwtBearerRefreshHandler(IOptions<JwtBearerRefreshOptions> refreshOptions
-            , IOptionsMonitor<JwtBearerOptions> options
+        public JwtBearerRefreshHandler(IOptionsMonitor<JwtBearerRefreshOptions> options
             , ILoggerFactory logger
             , UrlEncoder encoder
             , ISystemClock clock) : base(options, logger, encoder, clock)
         {
-            _options = refreshOptions.Value;
+            Events = _options.JwtBearerOptions.Events;
+        }
+
+        
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
